d9cb8ffca49b1938ad6d45a431a1d7216b845e0f
rjndev <nebrijaroyjoshua@gmail.com>
2023-03-23 09:53:17 +0800

    Changed backendUrl to production

--- src/backendConstant.js
+++ src/backendConstant.js
@@ -1,6 +1,6 @@
 const testUrl = "http://localhost:4000"
 const productionUrl = "https://amazonia-backend.onrender.com"
 
-const backendConnection = testUrl
+const backendConnection = productionUrl
 
 export default backendConnection
